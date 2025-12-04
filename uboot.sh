55536cf8bc2ab8bfa0d6fa2743a81cf1181dcb9e
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2025-12-04 23:40:56 +0100

    libretech-builder-simple.sh
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- uboot.sh
+++ uboot.sh
@@ -1,46 +1,26 @@
 #!/bin/bash
-set -x
-export CROSS_COMPILE=aarch64-linux-gnu-
-export ARCH=arm64
-OUTPUT=${PWD}/output
-mkdir -p ${OUTPUT}
-UBOOT=${PWD}/external/uboot  # Fixed: was pointing to wrong location
-mkdir -p ./external/uboot
 
 pushd ./external/uboot/
-	# Check for .git in the CURRENT directory
-	if [ ! -d ".git" ]; then
-		echo "Cloning u-boot..."
-		git clone https://github.com/libre-computer-project/libretech-u-boot .
-	else
-		echo "u-boot already exists — skipping clone"
-		git pull
-	fi
-	# Build U-boot
-	make aml-s905d3-cc_defconfig
-	#make -j$(nproc)
-	#make -j$(nproc) make DEBUG=1 PLAT=gxl AML_STDPARAMS=1
-	make PLAT=sm1 DEBUG=0
-	echo "U-Boot binary: ${PWD}/u-boot.bin"
+
+	# power off -> on the device
+	sudo uhubctl -l 1-3.4 -p 4 -a off; sleep 1; sudo uhubctl -l 1-3.4 -p 4 -a on
+
+	#install pre-requisites via apt or yum
+	sudo ./setup.sh
+
+	# ./build.sh BOARD_TARGET
+	# all-h3-cc-h3
+	# all-h3-cc-h5
+	# aml-s805x-ac
+	# aml-s905x-cc
+	# aml-s905d3-cc <------------------------
+	# aml-s905x-cc-v2
+	# aml-s905d-pc
+	# roc-rk3328-cc
+	# roc-rk3399-pc
+	./build.sh aml-s905d3-cc | tee build.log
+
+	# Deploy: put the device in usb mode by pressing the push button and then
+	# power the device
+	sudo /home/smalinux/.local/bin/boot-g12.py ./out/aml-s905d3-cc.usb.tpl
 popd
-
-pushd ./external/
-	# Check for amlogic-boot-fip
-	if [ ! -d "amlogic-boot-fip/.git" ]; then
-		echo "Cloning amlogic-boot-fip..."
-		git clone https://github.com/LibreELEC/amlogic-boot-fip --depth=1
-	else
-		echo "amlogic-boot-fip already exists — skipping clone"
-		cd amlogic-boot-fip
-		git pull
-		cd ..
-	fi
-
-	cd amlogic-boot-fip
-	# Use correct path to u-boot.bin
-	./build-fip.sh aml-s905d3-cc ${UBOOT}/u-boot.bin ${OUTPUT}
-	echo "Output directory: ${OUTPUT}"
-popd
-
-echo "Build complete!"
-echo "Bootloader image: ${OUTPUT}/u-boot.bin"
